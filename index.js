2e14d29ef8a2a194be8d8e366c2018cf11d5f4be
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-09 03:31:30 -0600

    Creates myFile.md

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the 'main#main' node
+let main = document.querySelector('main#main');
+main.remove();
+
+// Create a new 'h1' node, assign it to the 'newHeader' variable, and give it an id of 'victory'
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text inside the 'newHeader' node
+newHeader.textContent = 'RYAN-MONTGOMERY is the champion';
+
+// Append the 'newHeader' node to the body of the document
+document.body.appendChild(newHeader);
